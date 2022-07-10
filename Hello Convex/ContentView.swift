de79d5f343a09b07ddabd9fe2a042074e14e01c6
Lee Danilek <lee@convex.dev>
2022-07-10 12:02:30 -0700

    Initial Commit

---
+++ Hello Convex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hello Convex
+//
+//  Created by Lee Danilek on 7/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hello Convex/Hello_ConvexApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_ConvexApp.swift
+//  Hello Convex
+//
+//  Created by Lee Danilek on 7/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Hello_ConvexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
